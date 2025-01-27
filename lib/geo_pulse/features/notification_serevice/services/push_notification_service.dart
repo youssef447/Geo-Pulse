e2eca087dd21c0cd4830eaee3132be9858654075
youssef447 <youssef.ashraf380@gmail.com>
2025-01-27 21:42:36 +0200

    notifications refactors

--- lib/geo_pulse/features/notification_serevice/services/push_notification_service.dart
+++ lib/geo_pulse/features/notification_serevice/services/push_notification_service.dart
@@ -11,6 +11,7 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 
 import '../../notifications/data/models/notification_model.dart';
+import '../secret.dart';
 
 abstract class PushNotificationService {
   static final FirebaseFirestore _db = FirebaseFirestore.instance;
@@ -198,24 +199,6 @@ abstract class PushNotificationService {
   /// The token is valid for one hour. After that, the method needs to be called
   /// again to obtain a new token.
   static Future<String?> _getAccessToken() async {
-    final serviceAccountJson = {
-      "type": "service_account",
-      "project_id": "time-tracker-672cc",
-      "private_key_id": "113dec0293278c919dc6a246840fc97b7b19d5b8",
-      "private_key":
-          "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCipxustCVtNf/K\npcT6u/1puorL9lArpyWxQTB40eczz5bLYEavrBUAqiYF7majt1dzTVKTOj+Nz1Gh\ngTlFXRwqJhiQvtF5Kb3tU+CrkJ7V4PR+FwL8mMMJRetwMD/Jgf+C67KaH3paX20R\nZ7ht0xJfeL27m6jueIPqsM34t/MGovXbsEVofYPmub5ofYiB9o0gVVWCbiuvcTdZ\nT/+iC1IgdGwhafXweRSYfCJ9391B8NSLazrcJTcmI1gk/h4WfVanHDyuv4UiSCvO\n+XJ9eyusjJAmr4hGDEUkYV+8eKmXJ3Hoo1ze4LsFJB6co4o+DntVjmt2N9FBl4jz\nOwCaeeR1AgMBAAECggEAALKIutfW5T37B8poKs8NihuDptY6h8VisvzdkVcND7rP\nIbYa93vcZzFTkmjwRbluhY8KUJlKVnhSGEguj66ThVa+ejhnO4bDSrY2W2X2WGJr\nifLdT2n0RCDpnoSGcX2+M7iA/oouAx8vn4h7UPU7ue8RmTFUqxkXL7VsXKmlQB0H\nAYpGGYrXg0QVav530Yfr6+NhZPuVbdsn38MC8x1diy8secMUBhGmX74zwFNp2UDP\nvvZCgaE0mukbOgVMX5QLm5YpXw+zPGvScTW3KSK7pHJqb18l897x60Z5z38J6spY\n1spmFySl272a1hCwvKOqNe4M5ifOCkxyJzqVINf0wQKBgQDcaYTk1Y5YwaBa2bfC\npgUpCdchxuOsSPM21V2Z6HmMBxLl7HJC5Rov0KSTAPuL7mmlvEsbeYJTNP5F5J5V\nIPV1AQQhPjkbIwSgRJRWs89rVwiFF3xf2DbokR1HIE0w1Ry9N1mY5vHwKd7NcdNW\ntkmzVO4fmTFdtVQPM56yJv+wsQKBgQC86iu0jm6prh8koC+fmMNtXxm7JT2YgjOL\nEULUwXtPKgl2xyOfx1czrrgimBKfEVUTrzvwHL/UWUPr31OVJIXjzmPC3cimA8Qg\nAoHsjLqHr6euU+goaV8ebKSWW5+1Cq8HeJZmjKtf6Wzgf/h3jrDgDvcAD2bXqsu3\n/rZmqSbBBQKBgQCfO5QIhceqk2e3eqZo9uuvdC55dmgwpRsgOBDBCdQVt16NwL3j\nWgQLGx5qHUGdHAYy988C8EuYtSYldD52nbL2bl9/bKZw1mGXLzRDVu+4Sk4baJYv\n39H7Pir6oXlil/OFNyGBdwa+TGFr5pmZgWPJLMhDB7dETaWGvfIeHlKGYQKBgGsE\npTmlrl8FOrM+43ufyKW+yA7Sa/BnYdeYMang6X/RKodVDINJI8ctZTaDu5jM8ssx\n69x8DNe4sdd/LhRBGnAQzUbo6O/TFlmj0gI1Pr367hy9f7jU6IONvDdJNFIU+U5c\nK6dwbOzqiTkshY8FEIH9OEOadgIayUk0TBQgIsoZAoGAEl6+MJ3EobxhUnJVQ28i\nAfnKftu3jFQ0eUQqHUryqhi9MKpcNEbn3epupWvK9jRbgQ8X57z2RWHnBQ/YFLCL\n+STJEOg9qqU2wU/O6bkoL/Nc1qRxmqlMAvd+OlWs8NxuIjw/XgxW3N7oCDEExr4p\npnUHdX2D8z2a7HA2Fd/Ejfk=\n-----END PRIVATE KEY-----\n",
-      "client_email":
-          "firebase-adminsdk-8h6b3@time-tracker-672cc.iam.gserviceaccount.com",
-      "client_id": "111608450257597302771",
-      "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-      "token_uri": "https://oauth2.googleapis.com/token",
-      "auth_provider_x509_cert_url":
-          "https://www.googleapis.com/oauth2/v1/certs",
-      "client_x509_cert_url":
-          "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-8h6b3%40time-tracker-672cc.iam.gserviceaccount.com",
-      "universe_domain": "googleapis.com"
-    };
-
     List<String> scopes = [
       "https://www.googleapis.com/auth/userinfo.email",
       "https://www.googleapis.com/auth/firebase.database",
